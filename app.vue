eca40cb3842fe13170f8f8d101c9e77f0e5159cf
Emma Chen <weilin.emma.chen@gamil.com>
2023-07-14 14:02:11 +0800

    setup env

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- NAVBAR -->
+    <header class="sticky top-0 z-50 flex justify-between space-x-1 border-b p-4 shadow-md bg-white">
+      <a href="/" class="text-3xl font-mono">cartrader</a>
+    </header>
+    <!-- NAVBAR -->
+    <!-- HOME HERO -->
+    <div class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom">
+      <div class="bg-red-500 bg-cover relative"></div>
+    </div>
+
+    <!-- HOME HERO -->
+  </div>
+</template>
